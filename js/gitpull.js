24d5d1491f7ef40564c1b5faed8f6b5779ec9374
David Stevens <david.stevens@newspring.cc>
2014-03-19 21:44:25 -0400

    Added images & icons, updated copy

---
+++ js/gitpull.js
@@ -0,0 +1,10 @@
+/**
+ *	GitPull.js
+ *
+ *	@Author: David Stevens <dcs619@gmail.com> | http://twitter.com/davidc_stevens
+ *	@Requires: Nothing, vanilla Javascript o.O
+ */
+
+;(function(write) {
+	
+});
