d1d1beed47f259822f8cf53143277874b963f070
David Stevens <dcs619@gmail.com>
2018-06-18 14:49:03 -0400

    style tweaks

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,7 @@ const Hero = props => {
     <React.Fragment>
       <section className="hero">
         <h1>
-          This is a demo site of&nbsp;the <strong>heroBlog</strong> GatsbyJS starter
+          Hey there! <br />You've reached a work in progress...
         </h1>
         <button onClick={scrollToContent} aria-label="scroll">
           <FaArrowDown />
